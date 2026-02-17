399c07b4e9314a9db5eb5a5bbbf6bbcd0604d1c0
muhammadaus <aushijree@gmail.com>
2026-02-18 00:16:30 +0900

    Move PermissionedBToken to src with owner-only transfer restriction
    - Move from src/test/ to src/ as production contract
    - Use OpenZeppelin _beforeTokenTransfer hook pattern
    - Only owner can transfer tokens (holders cannot transfer)
    - Update deployment script import path

--- script/DeployPermissionedBToken.s.sol
+++ script/DeployPermissionedBToken.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import "../src/test/PermissionedBToken.sol";
+import "../src/PermissionedBToken.sol";
 
 contract DeployPermissionedBToken is Script {
     function run() external {
